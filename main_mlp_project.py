9f8806eecfcd9fbf0669bf6324fae6384c94989c
tgminski <tomgm@wp.pl>
2025-11-22 18:58:20 +0100

    Add files via upload

---
+++ main_mlp_project.py
@@ -0,0 +1,93 @@
+''' MLP project
+
+main file
+
+Paweł Józefczyk
+Tomasz Gmiński
+
+data file: 'cars.csv'
+
+As preliminary Project ML was used.
+    - github repository: https://github.com/tgminski/Projekt_ML_final
+    - files on folder: '\old_ML_procect_files\'
+
+'''
+
+
+# import libraries
+import utilites_TG
+import utilites_PJ
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+
+
+def main():
+
+#------------------------------------------------------------
+# 1. Read raw data
+#------------------------------------------------------------
+    # 1.1 Import from network (Keaggle)
+    # 1.2 Import from local file
+
+
+#------------------------------------------------------------
+# 2. Check data 
+#------------------------------------------------------------
+    # 2.1 Data description
+    # 2.2 Data 
+
+#------------------------------------------------------------
+# 3. Explore data
+#------------------------------------------------------------
+    # 3.1 NaN checking
+    # 3.2 Data distributions, transformations for normality
+    # 3.3 Outlaiers selection (outlaiers in one variable context - statistic )
+
+#------------------------------------------------------------
+# 4. ML modeling
+#------------------------------------------------------------
+    # 4.1 Base model
+    # 4.2 Features checking
+    # 4.3 Features selection
+    # 4.4 Outlaiers selection (outlaiers in ML context)
+    # 4.5 Model training
+
+#------------------------------------------------------------
+# 5. ML process optimisation
+#------------------------------------------------------------
+
+#------------------------------------------------------------
+# 6. Special functions
+#------------------------------------------------------------
+    # 6.1 ML using in ML function context
+        # 6.1.1 Data clustering (in ML context)
+        # 6.1.2 Outliers selection (in ML context)
+        # 6.1.3 ML modeling (in ML context)
+
+#------------------------------------------------------------
+# 7. Conclusions
+#------------------------------------------------------------
+
+
+    return []
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
